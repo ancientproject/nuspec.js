0a20ec0bc0208b1e3b3fd4d3f0b58f3e0846ce41
Yuuki Wesp <ls-micro@ya.ru>
2020-04-06 05:02:56 +0300

    add logic

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-import xml from "xml2js";
-
-
+import { parseStringPromise } from "xml2js";
+import { NuSpec, NuSpecFile } from "./nuspec";
+export default async (value): Promise<NuSpec> => {
+    if(!value)
+        throw new Error("Argument error. [value is not defined]");
+    if(typeof value != "string")
+        throw new Error("Argument error. [value is not string]");
+    let result = await parseStringPromise(value);
+    if(!result.package)
+        throw new Error("Invalid nuspec file. [Section 'package' is not defnied]");
+    let pkg = result.package;
+    let nuspec: NuSpec = { ...pkg };
+    return nuspec;
+}
